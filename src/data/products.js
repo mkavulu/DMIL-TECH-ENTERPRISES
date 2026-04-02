3ed8f26e2baf52d83cb928ba1597c3383204029d
Moses Kavulu <mutukukavulu2000@gmail.com>
2026-04-02 16:14:47 +0300

    made chages

--- src/data/products.js
+++ src/data/products.js
@@ -8,7 +8,7 @@ import hpDesktopImg from "../assets/hp-desktop.jpg";
 import dellDesktopImg from "../assets/dell-desktop.jpg";
 import lenovoLaptopImg from "../assets/lenovo-laptop.jpg";
 import earbudsImg from "../assets/earbuds.jpg";
-import allInOneImg from "../assets/All-In-One.jpg";
+import allInOneImg from "../assets/hp-all-in-one.jpeg";
 import laptopChargerImg from "../assets/laptop-charger.jpg";
 import routerImg from "../assets/router.png";
 import switchImg from "../assets/switches.jpeg";
@@ -20,6 +20,19 @@ import printersImg from "../assets/printer.jpg";
 import smartwatch2Img from "../assets/smartwatch2.jpeg";
 import smartwatch3Img from "../assets/smartwatch3.jpeg";
 import smartwatch4Img from "../assets/smartwatch4.jpeg";
+import GaminglaptopImg from "../assets/gaminglaptop.jpg";
+import lenovoDesktopImg from "../assets/lenovodesktop.jpg";
+import gamingDesktopImg from "../assets/gamingdesktop.jpg";
+import earbudImg from "../assets/earbuds 2.jpeg";
+import earbudzImg from "../assets/earbuds3.jpg";
+import earbudzzImg from "../assets/earbuds4.jpg";
+import DellallInOneImg from "../assets/dell-all-in-one.jpg";
+import lenovoallInOneImg from "../assets/lenovo-all-in-one.jpg";
+import dellmonitorImg from "../assets/dell-monitor.jpg";
+import lenovomonitorImg from "../assets/lenovo-monitor.jpg";
+import sonyheadphonesImg from "../assets/sony.jpg";
+import logitechheadphonesImg from "../assets/logitech.jpeg";
+
 
 const products = [
   { id: 1, name: "Wireless Headphones", category: "headphones", image: headphonesImg },
@@ -30,21 +43,32 @@ const products = [
   { id: 6, name: "Dell Laptop", category: "laptops", image: dellLaptopImg },
   { id: 7, name: "HP Desktop", category: "desktops", image: hpDesktopImg },
   { id: 8, name: "Dell Desktop", category: "desktops", image: dellDesktopImg },
-  { id: 9, name: "Ear Buds", category: "earbuds", image: earbudsImg },
+  { id: 9, name: "Audionic Ear Buds", category: "earbuds", image: earbudsImg },
   { id: 10, name: "Lenovo Laptop", category: "laptops", image: lenovoLaptopImg },
-  { id: 11, name: "All-In-One", category: "all-in-one", image: allInOneImg },
+  { id: 11, name: "HP All-In-One", category: "all-in-one", image: allInOneImg },
   { id: 12, name: "Laptop Charger", category: "accessories", image: laptopChargerImg },
   { id: 13, name: "Router", category: "accessories", image: routerImg },
   { id: 14, name: "Switch", category: "accessories", image: switchImg },
   { id: 15, name: "Toner & Cartridges", category: "printers", image: tonerImg },
   { id: 16, name: "HP Printer", category: "printers", image: printerImg },
   { id: 17, name: "Epson Printer", category: "printers", image: printersImg },
-  { id: 18, name: "Monitor", category: "monitors", image: monitorImg },
+  { id: 18, name: "HP Monitor", category: "monitors", image: monitorImg },
   { id: 19, name: "Wireless Keyboard & Mouse", category: "accessories", image: keyboardImg },
   { id: 20, name: "Smart Watch", category: "smartwatch", image: smartwatch2Img },
   { id: 21, name: "Smart Watch", category: "smartwatch", image: smartwatch3Img },
   { id: 22, name: "Smart Watch", category: "smartwatch", image: smartwatch4Img },
-  
+  { id: 23, name: "Gaming Laptop", category: "laptops", image: GaminglaptopImg },
+  { id: 24, name: "Lenovo Desktop", category: "desktops", image: lenovoDesktopImg },
+  { id: 25, name: "Gaming Desktop", category: "desktops", image: gamingDesktopImg },
+  { id: 26, name: "Apple Ear Buds", category: "earbuds", image: earbudImg },
+  { id: 27, name: "Oraimo Ear Buds", category: "earbuds", image: earbudzImg },
+  { id: 28, name: "Ear Buds", category: "earbuds", image: earbudzzImg },
+  { id: 29, name: "Dell All-In-One", category: "all-in-one", image: DellallInOneImg },
+  { id: 29, name: "Lenovo All-In-One", category: "all-in-one", image: lenovoallInOneImg },
+  { id: 30, name: "Dell Monitor", category: "monitors", image: dellmonitorImg },
+  { id: 31, name: "Lenovo Monitor", category: "monitors", image: lenovomonitorImg },
+  { id: 32, name: "Sony Headphones", category: "headphones", image: sonyheadphonesImg },
+  { id: 32, name: "Logitech Headphones", category: "headphones", image: logitechheadphonesImg },
 ];
 
 export default products;
